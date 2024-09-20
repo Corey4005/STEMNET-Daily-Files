f4ac5e60b8e928be595dac8adb687472210d23d2
Corey4005 <corey@DESKTOP-TL070SE.localdomain>
2024-09-20 11:54:13 -0500

    updated main to aut0omate filepaths

--- main.py
+++ main.py
@@ -4,8 +4,28 @@ Purpose: a script to drive the data infrustructure process.
 """
 import subprocess
 import time
+import os 
 
 def main():
+    # make sure the directories exist if they do not already 
+    cwd = os.getcwd()
+    daily_files_dir = os.path.join(cwd, 'daily_files')
+    logfiles_dir = os.path.join(cwd, 'logfiles')
+    last_station_times_dir = os.path.join(logfiles_dir, 'last_station_times')
+    station_data_dir = os.path.join(cwd, 'station_data')
+    metadata_dir = os.path.join(cwd, 'metadata')
+
+    if not os.path.exists(daily_files_dir):
+        os.mkdir(daily_files_dir)
+    if not os.path.exists(logfiles_dir):
+        os.mkdir(logfiles_dir)
+    if not os.path.exists(last_station_times_dir):
+        os.mkdir(last_station_times_dir)
+    if not os.path.exists(station_data_dir):
+        os.mkdir(station_data_dir)
+    if not os.path.exists(metadata_dir):
+        os.mkdir(metadata_dir)
+
     start = time.time()
     #fetch the updated metadata and all available soil moisture data
     subprocess.run(['python', './get_data.py'])
