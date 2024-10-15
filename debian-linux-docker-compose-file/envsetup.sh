1b402e34a3822e4a8386aeba9ec60a66a438033e
corey4005 <corey4005@gmail.com>
2024-10-14 19:27:24 -0500

    updated environment steps

--- debian-linux-docker-compose-file/envsetup.sh
+++ debian-linux-docker-compose-file/envsetup.sh
@@ -3,6 +3,6 @@
 USER_ENV=$(id -u $USER)
 echo "setting USER variable in env_file.env to $USER_ENV"
 #clear the file 
-: > ./environment/env_file.env
+: > ./.env
 #set the USER variable 
-echo "LOCAL_USER_ID=$USER_ENV" >> ./environment/env_file.env
+echo "LOCAL_USER_ID=$USER_ENV" >> ./.env
